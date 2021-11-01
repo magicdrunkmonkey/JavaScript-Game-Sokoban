f1cc04ca4e8a742c3a0c09babbdfae5402c4652f
George Sydby <george.sydby@gmail.com>
2021-11-01 20:50:21 +0100

    Massa experiment med fattar inte!!

--- JavaScript/utilsFunction.js
+++ JavaScript/utilsFunction.js
@@ -1,23 +1,56 @@
 // JavaScript source code
 
 // Store the players X & Y position in global variables.
-var player;
+var player_X_position;
+var player_Y_position;
+
+/* Attempt 1 Grid fail : Creating JavaScript constructor
+export default class MapGrid
+{
+    constructor(root)
+    {
+        this.root = root;
+        this.root.innerHTML = `
+                            
+        `;
+    }
+}*/
+
+
 
 // 5. Create a function to generate the elements that will represent the tiles from the map array.
 //      a) Give them an id and CSS.
 //      b) Append them into your map element.
 
+
+
+var mapElements = [];
+// Från demo med Simon
 function drawBoard(tileMap) {       //Skriver ut innehållet i arrayerna som innehåller kartbitarna 
     for (let x = 0; x < tileMap.width; x++) {
+        //document.getElementById("map").innerHTML = x; //Funkar inte
+        let mapElements = [];
+        
         for (let y = 0; y < tileMap.height; y++) {
-            console.log("x: " + x + " y: " + y);      //Visar bitarna i webläsar konsollen
+            console.log("x: " + x + " y: " + y);      //Visar kartpositioner i webläsarens console
+            //document.getElementById("map").innerHTML = y;  //Funkar inte
+            mapElements.push(tileMap[x]);            
         }
+        document.getElementById("map").innerHTML = mapElements;
     }
+    //document.getElementById("map").innerHTML = mapElements;
 }
 
 drawBoard(tileMap01);     //Kallar på funktionen drawBoard med inparametern tileMap01 som fanns i SokobanBase.js
 
-
+/*  Attempt 1 Board Funkar inte
+function drawBoardTest1(tileMapTest1)
+{
+    for (let x = 0; x < tileMap.width; x++) 
+    {
+        document.getElementById("map").innerHTML = tileMapTest1[i]
+    }
+}*/
 
 function moveUp() {
      
