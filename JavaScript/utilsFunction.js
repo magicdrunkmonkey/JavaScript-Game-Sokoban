f6cade2328867f4a10364dc6c60a84b9291f12b2
George Sydby <george.sydby@gmail.com>
2021-11-01 17:01:49 +0100

    Tessting again

--- JavaScript/utilsFunction.js
+++ JavaScript/utilsFunction.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 // 5. Create a function to generate the elements that will represent the tiles from the map array.
-//      Give them an id and CSS.
-//      Append them into your map element.
+//      a) Give them an id and CSS.
+//      b) Append them into your map element.
 
 function drawBoard(tileMap) {
     for (let x = 0; x < tileMap.width; x++) {
